c22c2ae564529aa5746f9a4d0b02cf1a4c7a26c7
AkidSe <akidse@mail.ru>
2018-05-14 12:13:55 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from './auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AuthService]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Finance Manager';
+
+  constructor(public authService: AuthService){}
+
+  ngOnInit() {
+  }
+
+  logOut (): void{
+  	this.authService.logOut();
+  }
 }
